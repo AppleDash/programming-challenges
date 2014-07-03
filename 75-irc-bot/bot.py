d0a43d858440d297d4d3a44d133539dde490de5b
AppleDash <derp@derpy.fr>
2014-07-03 02:21:18 -0400

    Improve IRC bot

--- 75-irc-bot/bot.py
+++ 75-irc-bot/bot.py
@@ -2,68 +2,73 @@
 import sys
 import socket
 
-def handle_privmsg(config, sock, line, spl):
-    sender = spl[0][1:].split("!")[0]  # Parse out the message sender's nick
-    target = spl[2]
-    message = line[1:].split(":")[1].strip() # Parse out the message; this won't account for colons in the body, but this is quick.
-    reply_to = sender
-    if target[0] == "#":
-        reply_to = target
 
-    if message.lower() == ("hi, %s" % config["me"]["nick"].lower()):
-        sock.send("PRIVMSG %s :Hello there, %s!\r\n" % (reply_to, sender))
 
-    if message[0] == "!":
-        data = message[1:].split(" ")
-        command = data[0].lower()
-        args = []
-        if len(data) > 1:
-            args = data[1:]
 
-        if command == "say":
-            if len(args) == 0:
-                sock.send("PRIVMSG %s :You need to specify something to say!\r\n")
-            else:
-                sock.send("PRIVMSG %s :%s\r\n" % (reply_to, " ".join(args)))
 
-        # Add more bot commands here!
+class Bot:
+    def __init__(self, host, port, nick, channel):
+        self.host = host
+        self.port = port
+        self.nick = nick
+        self.channel = channel
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-def run_bot(config):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((config["server"]["host"], config["server"]["port"]))
-    sock.send("NICK %s\r\nUSER python * * :Python\r\n" % config["me"]["nick"])
+    def sendraw(self, data):
+        self.sock.send(data + "\r\n")
 
-    for line in sock.makefile("r"):
-        line = line.strip()
-        print(line)
-        spl = line.split(" ")
+    def run(self):
+        self.sock.connect((self.host, self.port))
+        self.sendraw("NICK %s" % self.nick)
+        self.sendraw("USER python * * :Python")
+    
+        for line in self.sock.makefile("r"):
+            line = line.strip()
+            print("-> " + line)
+            spl = line.split(" ")
+    
+            if spl[0] == "PING":
+                self.sendraw(line.replace("PING", "PONG"))
+    
+            if spl[1] == "376":  # End of MOTD
+                self.sendraw("JOIN %s" % self.channel)
+            elif spl[1] == "PRIVMSG":
+                self.handle_privmsg(line, spl)
 
-        if spl[0] == "PING":
-            sock.send(line.replace("PING", "PONG") + "\r\n")
-
-        if spl[1] == "376":  # End of MOTD
-            sock.send("JOIN %s\r\n" % config["me"]["channel"])
-        elif spl[1] == "PRIVMSG":
-            handle_privmsg(config, sock, line, spl)
+    def handle_privmsg(self, line, spl):
+        sender = spl[0][1:].split("!")[0]  # Parse out the message sender's nick
+        target = spl[2]
+        message = line[1:][line[1:].index(":")+1:] # Parse out the message
+        print("[%s] <%s> %s" % (target, sender, message))
+        reply_to = sender
+        if target[0] == "#":
+            reply_to = target
+    
+        if message.lower() == ("hi, %s" % self.nick.lower()):
+            self.sendraw("PRIVMSG %s :Hello there, %s!" % (reply_to, sender))
+    
+        if message[0] == "!":
+            data = message[1:].split(" ")
+            command = data[0].lower()
+            args = []
+            if len(data) > 1:
+                args = data[1:]
+    
+            if command == "say":
+                if len(args) == 0:
+                    self.sendraw("PRIVMSG %s :You need to specify something to say!")
+                else:
+                    self.sendraw("PRIVMSG %s :%s" % (reply_to, " ".join(args)))
+    
+            # Add more bot commands here!   
 
 def main(argv):
     if len(argv) < 5:
         print("Usage: %s <IRC server host> <IRC server port> <bot nick> <channel to join>" % argv[0])
         return 1
 
-    config = {
-        "server": {
-            "host": argv[1],
-            "port": int(argv[2])
-        },
-
-        "me": {
-            "nick": argv[3],
-            "channel": argv[4]
-        }
-    }
-
-    run_bot(config)
+    b = Bot(argv[1], int(argv[2]), argv[3], argv[4])
+    b.run()
 
     return 0
 
